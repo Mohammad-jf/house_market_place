1215f1d4fdc52d908cd94e56e9788dd8bd8a9038
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-08-11 07:23:26 +0330

    8.forgot password

--- src/pages/FrogotPassword.jsx
+++ src/pages/FrogotPassword.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { ReactComponent as ArrowRight } from '../assets/svg/keyboardArrowRightIcon.svg';
+
 
 const FrogotPassword = () => {
+  const [email, setEmail] = useState('');
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success('Email Was Sent');
+      setEmail('');
+
+    } catch (err) {
+      toast.error('Could Not Send Reset Email')
+    }
+  }
+
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+
   return (
-    <div>Frogot Password</div>
+    <div className='pageContainer'>
+      <header>
+        <p className="pageHeader">Forgot Password </p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            placeholder='email'
+            className='emailInput'
+            onChange={onChange} />
+
+          <Link className='forgotPasswordLink' to='/sign-in'>Sign In</Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link </div>
+            <button className="signInButton">
+              <ArrowRight fill='white' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+      </main>
+
+    </div>
   )
 }
 
